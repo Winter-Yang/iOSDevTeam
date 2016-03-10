2ce48405d2533f6500276ed6c23c780a34a3521e
joinup <shen_sgh@icloud.com>
2016-03-10 13:43:07 +0800

    create

---
+++ iOSDevTeam/iOSDevTeam/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSDevTeam
+//
+//  Created by shen_gh on 16/3/10.
+//  Copyright © 2016年 com.joinup(Beijing). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSDevTeam/iOSDevTeam/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSDevTeam
+//
+//  Created by shen_gh on 16/3/10.
+//  Copyright © 2016年 com.joinup(Beijing). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
